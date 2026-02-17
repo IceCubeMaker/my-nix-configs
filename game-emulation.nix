fa36e9495a55f37cd55bfd2559982bc1375f1041
franz <franzluehr@gmail.com>
2026-02-17 09:54:30 +0100

    Added More Emulators

--- game-emulation.nix
+++ game-emulation.nix
@@ -5,23 +5,32 @@ let
 
   # Map platforms to their specific launch commands
   platformMap = {
-    "snes"     = "retroarch -L /run/current-system/sw/lib/retroarch/cores/snes9x_libretro.so";
-    "gba"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/mgba_libretro.so";
-    "nes"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/nestopia_libretro.so";
-    "n64"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/parallel_n64_libretro.so";
-    "gc"       = "dolphin-emu -e";
-    "wii"      = "dolphin-emu -e";
-    "wiiu"     = "cemu -g";
-    "switch"   = "Ryujinx";
-    "gb"       = "retroarch -L /run/current-system/sw/lib/retroarch/cores/mgba_libretro.so";
-    "gbc"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/mgba_libretro.so";
-    "3ds"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/citra_libretro.so";
-    "gen"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/genesis_plus_gx_libretro.so";
-    "sat"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/yabause_libretro.so";
-    "psx"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/pcsx_rearmed_libretro.so";
-    "psp"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/ppsspp_libretro.so";
-    "nds"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/desmume_libretro.so";
-    "dos"      = "retroarch -L /run/current-system/sw/lib/retroarch/cores/dosbox_libretro.so";
+    "snes"             = "retroarch -L /run/current-system/sw/lib/retroarch/cores/snes9x_libretro.so";
+    "gba"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/mgba_libretro.so";
+    "nes"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/nestopia_libretro.so";
+    "n64"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/parallel_n64_libretro.so";
+    "gc"               = "dolphin-emu -e";
+    "wii"              = "dolphin-emu -e";
+    "wiiu"             = "cemu -g";
+    "switch"           = "Ryujinx";
+    "gb"               = "retroarch -L /run/current-system/sw/lib/retroarch/cores/mgba_libretro.so";
+    "gbc"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/mgba_libretro.so";
+    "3ds"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/citra_libretro.so";
+    "megadrive"        = "retroarch -L /run/current-system/sw/lib/retroarch/cores/genesis_plus_gx_libretro.so";
+    "saturn"           = "retroarch -L /run/current-system/sw/lib/retroarch/cores/yabause_libretro.so";
+    "psx"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/pcsx_rearmed_libretro.so";
+    "psp"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/ppsspp_libretro.so";
+    "nds"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/desmume_libretro.so";
+    "pc"               = "retroarch -L /run/current-system/sw/lib/retroarch/cores/dosbox_libretro.so";
+    "neogeo"           = "retroarch -L /run/current-system/sw/lib/retroarch/cores/fbneo_libretro.so";
+    "mame"             = "retroarch -L /run/current-system/sw/lib/retroarch/cores/mame_libretro.so";
+    "gamegear"         = "retroarch -L /run/current-system/sw/lib/retroarch/cores/genesis_plus_gx_libretro.so";
+    "wonderswancolor"  = "retroarch -L /run/current-system/sw/lib/retroarch/cores/beetle_wswan_libretro.so";
+    "ps2"              = "retroarch -L /run/current-system/sw/lib/retroarch/cores/pcsx2_libretro.so";
+    "dreamcast"        = "retroarch -L /run/current-system/sw/lib/retroarch/cores/flycast_libretro.so";
+    "atari2600"        = "retroarch -L /run/current-system/sw/lib/retroarch/cores/stella_libretro.so";
+    "pcengine"         = "retroarch -L /run/current-system/sw/lib/retroarch/cores/beetle_pce_fast_libretro.so";
+    "mastersystem"     = "retroarch -L /run/current-system/sw/lib/retroarch/cores/genesis_plus_gx_libretro.so";
   };
 
   pegasusRules = map (p: "w+ /home/franz/.config/pegasus-frontend/game_dirs.txt - - - - /home/franz/Games/ROMs/${p}/.metadata\\n") platforms;
@@ -56,13 +65,22 @@ let
         ;;
     esac
 
-    mkdir -p "$OUT_DIR"vice_libretr
+    mkdir -p "$OUT_DIR"
     
     echo "Fetching data for $PLATFORM from ScreenScraper..."
-    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s screenscraper -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s igdb -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s screenscraper -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend,onlymissing
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s tgdb -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend,onlymissing
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s mobygames -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend,onlymissing
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s openretro -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend,onlymissing
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s arcadedb -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend,onlymissing
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s zxinfo -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend,onlymissing
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s gamebase -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend,onlymissing
+    ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -s esgamelist -i "$ROM_DIR" -g "$OUT_DIR" --flags unattend,onlymissing
 
     echo "Generating Pegasus metadata for $PLATFORM..."
     ${pkgs.skyscraper}/bin/Skyscraper -p "$PLATFORM" -f pegasus -i "$ROM_DIR" -o "$OUT_DIR" -g "$OUT_DIR" -e "$LAUNCH_CMD" --flags unattend
+    
   '';
   
   ######################
@@ -98,9 +116,15 @@ in {
     libretro.citra             # 3DS
     libretro.pcsx_rearmed      # Playstation 1
     libretro.ppsspp            # Playstation Portabl
-    libretro.genesis-plus-gx   # Gensis / Megadrive
+    libretro.genesis-plus-gx   # Gensis / Megadrive, Master System
     libretro.picodrive         # Genesis, Sega CD, 32X
     libretro.dosbox            # DOS
+    libretro.fbneo             # Neo Geo
+    libretro.mame              # Arcade (MAME)
+    libretro.beetle-pce-fast   # WonderSwan Colour, TurboGrafx-16
+    libretro.pcsx2             # Playstation 2
+    libretro.flycast           # Dreamcast
+    libretro.stella            # Atari 2600
 
     # Launcher
     pegasus-frontend
@@ -189,12 +213,6 @@ in {
   ] 
   ++ romDirRules 
   ++ pegasusRules;
-
-  environment.etc."skyscraper/confd1a9dcig.ini".text = ''
-    [main]
-    frontend="pegasus"
-    unattended="true"
-  '';
 
   systemd.user.services.pegasus-autoscrape = {
     description = "Automatically scrape ROM metadata for Pegasus";
